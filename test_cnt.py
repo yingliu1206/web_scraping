d3f2968760398f061a4dcf84d58fa2e4befc55a5
Ying Liu <71619071+yingliu1206@users.noreply.github.com>
2022-02-08 11:07:03 -0500

    Update test_cnt.py

--- test_cnt.py
+++ test_cnt.py
@@ -1,24 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import pymongo
 import pandas as pd
+from urllib.parse import urlparse
 
 if __name__ == '__main__':
-    charter2019_df = pd.read_csv('./scrapy/schools/schools/spiders/charter_school_URLs_2019.tsv', sep='\t')
-    print(charter2019_df, 6)
     
+    # read the charter school as a data frame
+    charter2019_df = pd.read_csv('./scrapy/schools/schools/spiders/charter_school_URLs_2019.tsv', sep='\t')
+    
+    # get a list of domains from charter school urls
+    domain_2019 = []
+    for i in charter2019_df['URL_2019']:
+        domain_origin = urlparse(i).netloc
+        domain_2019.append(domain_origin)
+    
+    # connect to mongodb and extract scraped urls
     client = pymongo.MongoClient('mongodb://localhost:27000',
                                  username= 'admin',
                                  password='mdipass'
                                 ) 
     db = client["schoolSpider"]
-    
-    # collection names in the database "schoolSpider"
     col = 'text'
     
     collection = db[col]
     cursor = collection.find({},{"url":1})
     
-    url_list = []
+    # get a list of domains from scraped urls
+    db_url_domain = []
     for ducument in cursor:
-        url_list.append(ducument['url'])
-    
-    print(len(url_list))
+        domain = urlparse(ducument['url']).netloc
+        db_url_domain.append(domain)
+
+    for i in domain_2019:
+        if i not in db_url_domain:
+            print(i)
+        
