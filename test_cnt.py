ba2212153d8d99ab46ee9569dfd058901bb572ff
Ying Liu <71619071+yingliu1206@users.noreply.github.com>
2022-02-08 10:08:28 -0500

    Update test_cnt.py

--- test_cnt.py
+++ test_cnt.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import pymongo
-
+import pandas as pd
 
 if __name__ == '__main__':
+    charter2019_df = pd.read_csv('./schools/spiders/charter_school_URLs_2019.tsv', sep='\t')
+    print(charter2019_df, 6)
+    
     client = pymongo.MongoClient('mongodb://localhost:27000',
                                  username= 'admin',
                                  password='mdipass'
@@ -15,4 +16,9 @@ if __name__ == '__main__':
     
     collection = db[col]
     cursor = collection.find({},{"url":1})
-    print(type(cursor))
+    
+    url_list = []
+    for ducument in cursor:
+        url_list.append(ducument['url'])
+    
+    print(len(url_list))
