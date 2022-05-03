a40984be871138a68fc3768da5eb859e04919539
Ying Liu <71619071+yingliu1206@users.noreply.github.com>
2022-05-03 10:18:37 -0400

    Update pipelines.py

--- schools/schools/pipelines.py
+++ schools/schools/pipelines.py
@@ -32,112 +32,112 @@ import mimetypes
 import requests
 import gridfs
 
-# class MongoDBImagesPipeline(object):
+class MongoDBImagesPipeline(object):
 
-#     def __init__(self, MONGO_URI, MONGODB_DB, MONGODB_COLLECTION_IMAGES, MONGO_USERNAME='admin', MONGO_PASSWORD=''):
-#         self.MONGO_URI = MONGO_URI
-#         self.MONGODB_DB = MONGODB_DB
-#         self.MONGODB_COLLECTION_IMAGES = MONGODB_COLLECTION_IMAGES
-#         self.MONGO_USERNAME = MONGO_USERNAME
-#         self.MONGO_PASSWORD = MONGO_PASSWORD
+    def __init__(self, MONGO_URI, MONGODB_DB, MONGODB_COLLECTION_IMAGES, MONGO_USERNAME='admin', MONGO_PASSWORD=''):
+        self.MONGO_URI = MONGO_URI
+        self.MONGODB_DB = MONGODB_DB
+        self.MONGODB_COLLECTION_IMAGES = MONGODB_COLLECTION_IMAGES
+        self.MONGO_USERNAME = MONGO_USERNAME
+        self.MONGO_PASSWORD = MONGO_PASSWORD
     
-#     @classmethod
-#     def from_crawler(cls, crawler):
-#         # pull in information from settings.py
-#         return cls(
-#             MONGO_URI=crawler.settings.get('MONGO_URI'),
-#             MONGODB_DB=crawler.settings.get('MONGODB_DB'),
-#             MONGODB_COLLECTION_IMAGES=crawler.settings.get('MONGODB_COLLECTION_IMAGES'),
-#             MONGO_USERNAME = crawler.settings.get('MONGO_USERNAME'),
-#             MONGO_PASSWORD = crawler.settings.get('MONGO_PASSWORD')
-#         )
+    @classmethod
+    def from_crawler(cls, crawler):
+        # pull in information from settings.py
+        return cls(
+            MONGO_URI=crawler.settings.get('MONGO_URI'),
+            MONGODB_DB=crawler.settings.get('MONGODB_DB'),
+            MONGODB_COLLECTION_IMAGES=crawler.settings.get('MONGODB_COLLECTION_IMAGES'),
+            MONGO_USERNAME = crawler.settings.get('MONGO_USERNAME'),
+            MONGO_PASSWORD = crawler.settings.get('MONGO_PASSWORD')
+        )
     
         
-#     def process_item(self, item, spider):
-#         valid = True
-#         for data in item:
-#             if not data:
-#                 valid = False
-#                 raise DropItem("Missing {0}!".format(data))
-#         if valid:
-#             connection = pymongo.MongoClient(
-#                 self.MONGO_URI,
-#                 username=self.MONGO_USERNAME, 
-#                 password=self.MONGO_PASSWORD
-#             )
-#             self.db = connection[self.MONGODB_DB]
-#             print("CONNECTED TO MONGO DB")
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            connection = pymongo.MongoClient(
+                self.MONGO_URI,
+                username=self.MONGO_USERNAME, 
+                password=self.MONGO_PASSWORD
+            )
+            self.db = connection[self.MONGODB_DB]
+            print("CONNECTED TO MONGO DB")
 
-#             #self.collection = self.db[self.MONGODB_COLLECTION_IMAGES]
-#             self.grid_fs = gridfs.GridFS(self.db, collection = self.MONGODB_COLLECTION_IMAGES)
+            #self.collection = self.db[self.MONGODB_COLLECTION_IMAGES]
+            self.grid_fs = gridfs.GridFS(self.db, collection = self.MONGODB_COLLECTION_IMAGES)
             
-#             links = item['image_urls']
+            links = item['image_urls']
 
-#             for link in links:
-#                 mime_type = mimetypes.guess_type(link)[0]
-#                 request = requests.get(link, stream=True)
-#                 self.grid_fs.put(request.raw, contentType=mime_type,
-#                     user = spider.user if hasattr(spider,"user") else None, 
-#                     rq_id = spider.rq_id if hasattr(spider,"rq_id") else None, 
-#                     filename = os.path.basename(link), bucketName = "images")
+            for link in links:
+                mime_type = mimetypes.guess_type(link)[0]
+                request = requests.get(link, stream=True)
+                self.grid_fs.put(request.raw, contentType=mime_type,
+                    user = spider.user if hasattr(spider,"user") else None, 
+                    rq_id = spider.rq_id if hasattr(spider,"rq_id") else None, 
+                    filename = os.path.basename(link), bucketName = "images")
        
-#             logging.debug(f"MongoDB: Inserted {item['image_urls']}.")
+            logging.debug(f"MongoDB: Inserted {item['image_urls']}.")
         
-#         return item
+        return item
     
     
     
-# class MongoDBFilesPipeline(object):
+class MongoDBFilesPipeline(object):
 
-#     def __init__(self, MONGO_URI, MONGODB_DB, MONGODB_COLLECTION_FILES, MONGO_USERNAME='admin', MONGO_PASSWORD=''):
-#         self.MONGO_URI = MONGO_URI
-#         self.MONGODB_DB = MONGODB_DB
-#         self.MONGODB_COLLECTION_FILES = MONGODB_COLLECTION_FILES
-#         self.MONGO_USERNAME = MONGO_USERNAME
-#         self.MONGO_PASSWORD = MONGO_PASSWORD
+    def __init__(self, MONGO_URI, MONGODB_DB, MONGODB_COLLECTION_FILES, MONGO_USERNAME='admin', MONGO_PASSWORD=''):
+        self.MONGO_URI = MONGO_URI
+        self.MONGODB_DB = MONGODB_DB
+        self.MONGODB_COLLECTION_FILES = MONGODB_COLLECTION_FILES
+        self.MONGO_USERNAME = MONGO_USERNAME
+        self.MONGO_PASSWORD = MONGO_PASSWORD
     
-#     @classmethod
-#     def from_crawler(cls, crawler):
-#         # pull in information from settings.py
-#         return cls(
-#             MONGO_URI=crawler.settings.get('MONGO_URI'),
-#             MONGODB_DB=crawler.settings.get('MONGODB_DB'),
-#             MONGODB_COLLECTION_FILES=crawler.settings.get('MONGODB_COLLECTION_FILES'),
-#             MONGO_USERNAME = crawler.settings.get('MONGO_USERNAME'),
-#             MONGO_PASSWORD = crawler.settings.get('MONGO_PASSWORD')
-#         )
+    @classmethod
+    def from_crawler(cls, crawler):
+        # pull in information from settings.py
+        return cls(
+            MONGO_URI=crawler.settings.get('MONGO_URI'),
+            MONGODB_DB=crawler.settings.get('MONGODB_DB'),
+            MONGODB_COLLECTION_FILES=crawler.settings.get('MONGODB_COLLECTION_FILES'),
+            MONGO_USERNAME = crawler.settings.get('MONGO_USERNAME'),
+            MONGO_PASSWORD = crawler.settings.get('MONGO_PASSWORD')
+        )
     
         
-#     def process_item(self, item, spider):
-#         valid = True
-#         for data in item:
-#             if not data:
-#                 valid = False
-#                 raise DropItem("Missing {0}!".format(data))
-#         if valid:
-#             connection = pymongo.MongoClient(
-#                 self.MONGO_URI,
-#                 username=self.MONGO_USERNAME, 
-#                 password=self.MONGO_PASSWORD
-#             )
-#             self.db = connection[self.MONGODB_DB]
-#             print("CONNECTED TO MONGO DB")
-#             #self.collection = self.db[self.MONGODB_COLLECTION_FILES]
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            connection = pymongo.MongoClient(
+                self.MONGO_URI,
+                username=self.MONGO_USERNAME, 
+                password=self.MONGO_PASSWORD
+            )
+            self.db = connection[self.MONGODB_DB]
+            print("CONNECTED TO MONGO DB")
+            #self.collection = self.db[self.MONGODB_COLLECTION_FILES]
 
-#             self.grid_fs = gridfs.GridFS(self.db, collection = self.MONGODB_COLLECTION_FILES)
+            self.grid_fs = gridfs.GridFS(self.db, collection = self.MONGODB_COLLECTION_FILES)
             
-#             links = item['file_urls']
-#             for link in links:
-#                 mime_type = mimetypes.guess_type(link)[0]
-#                 request = requests.get(link, stream=True)
-#                 self.grid_fs.put(request.raw, contentType=mime_type, 
-#                     user = spider.user if hasattr(spider,"user") else None, 
-#                     rq_id = spider.rq_id if hasattr(spider,"rq_id") else None, 
-#                     filename = os.path.basename(link), bucketName = "files")
+            links = item['file_urls']
+            for link in links:
+                mime_type = mimetypes.guess_type(link)[0]
+                request = requests.get(link, stream=True)
+                self.grid_fs.put(request.raw, contentType=mime_type, 
+                    user = spider.user if hasattr(spider,"user") else None, 
+                    rq_id = spider.rq_id if hasattr(spider,"rq_id") else None, 
+                    filename = os.path.basename(link), bucketName = "files")
        
-#             logging.debug(f"MongoDB: Inserted {item['file_urls']}.")
+            logging.debug(f"MongoDB: Inserted {item['file_urls']}.")
         
-#         return item
+        return item
     
 
 class MongoDBTextPipeline(object):
